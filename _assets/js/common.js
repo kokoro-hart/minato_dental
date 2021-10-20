ff0e88add23e34b34a7b49ff6a666905fb5a266e
kokoro <cocosyu.11261126@gmail.com>
2021-10-20 12:38:19 +0900

    Add:ビューポート幅360px以下はリサイズして表示

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -7,3 +7,19 @@
 //import $ from "@modules/jquery";
 //slickを使うとき
 // import '@modules/slick-carousel';
+
+//ビューポート幅360px以下はリサイズして表示
+!(function () {
+  const viewport = document.querySelector('meta[name="viewport"]');
+  function switchViewport() {
+    const value =
+      window.outerWidth > 360
+        ? 'width=device-width,initial-scale=1'
+        : 'width=360';
+    if (viewport.getAttribute('content') !== value) {
+      viewport.setAttribute('content', value);
+    }
+  }
+  addEventListener('resize', switchViewport, false);
+  switchViewport();
+})();
