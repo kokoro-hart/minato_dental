f2cb7ce0acf39abb212fc9931c0356359bf17f57
kokoro <cocosyu.11261126@gmail.com>
2021-10-24 17:56:42 +0900

    Add: タッチデバイスの場合hoverは無効

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -23,3 +23,22 @@
   addEventListener('resize', switchViewport, false);
   switchViewport();
 })();
+
+// タッチデバイスの場合:hover無効化
+const touch = 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+if (touch) {
+  try {
+    for (let si in document.styleSheets) {
+      const styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (let ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) { }
+}
