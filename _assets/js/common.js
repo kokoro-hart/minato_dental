00a84042d486db9295219fb9b64d865b50189179
kokoro <cocosyu.11261126@gmail.com>
2021-10-30 17:33:07 +0900

    Add: ページ内リンクイベントを追加

--- _assets/js/common.js
+++ _assets/js/common.js
@@ -70,3 +70,23 @@ function inViewport(entries, observer) {
     }
   });
 }
+
+//ページ内リンク
+window.addEventListener('DOMContentLoaded', () => {
+  const anchorLinks = document.querySelectorAll('a[href^="#"]');
+  const anchorLinksArr = Array.prototype.slice.call(anchorLinks);
+
+  anchorLinksArr.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.hash;
+      const targetElement = document.querySelector(targetId);
+      const targetOffsetTop = window.pageYOffset + targetElement.getBoundingClientRect().top - 60;
+
+      window.scrollTo({
+        top: targetOffsetTop,
+        behavior: "smooth"
+      });
+    });
+  });
+});
