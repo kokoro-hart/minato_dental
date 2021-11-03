28cad2261151d8863481fb302e44dad638480181
kokoro <cocosyu.11261126@gmail.com>
2021-11-03 23:13:46 +0900

    Add: メインビジュアルswiperのオプションを追加

--- _assets/js/modules/swiper.js
+++ _assets/js/modules/swiper.js
@@ -1,9 +1,38 @@
 
+// メインビジュアル
+if (document.getElementById('js-mv')) {
+  const swiperMv = new Swiper('.swiper-mv', {
+    // Optional parameters
+    speed: 400, // スライドが切り替わる時の速さ
+    slidesPerView: '1', // スライド表示数
+    effect: "fade", // フェードさせる
+    loop: true,
+    autoplay: { // 自動再生 ON
+      delay: 4000, // 次のスライドまでの秒数
+      disableOnInteraction: false, //スライダー操作後、自動再生が止まるかどうか
+    },
+    fadeEffect: {
+      crossFade: true // クロスフェードさせる
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+}
+
 
 // スタッフ紹介ページギャラリー
-//--------------------------------------------------------------------------
-if (document.URL.match(/staff/)) {
-  const swiper = new Swiper('.swiper-gallery', {
+if (document.getElementById('js-gallery')) {
+  const swiperGallery = new Swiper('.swiper-gallery', {
 
     speed: 9000,
     slidesPerView: '2',
