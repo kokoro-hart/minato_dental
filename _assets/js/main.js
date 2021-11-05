a41bcac88e6cf03fcd9e2f2406bfc0359e5b3b55
kokoro <cocosyu.11261126@gmail.com>
2021-11-05 23:28:19 +0900

    Add: plyfillの読み込み

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,8 +1,24 @@
-//svguse（svgスプライト ie対応）
+//ie対応
+
+//svgスプライト
 import '@modules/svgxuse';
+
+//object-fit
+import '@modules/object-fit-images';
+
+//intersection observer
+import '@modules/intersection-observer';
+
+//picture
+import '@modules/picturefill';
+
+//line-clamp
+import './lib/clamp.js';
 
 //ブラウザ判定
 import './lib/b_browser_switcher.js';
+
+
 
 import './common'
 
