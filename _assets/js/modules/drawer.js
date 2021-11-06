d5359006ef44b67c0365da36c5e7c9e621cd0e3d
kokoro <cocosyu.11261126@gmail.com>
2021-11-06 10:44:09 +0900

    Fix: jsDrawerButton -> drawerButton

--- _assets/js/modules/drawer.js
+++ _assets/js/modules/drawer.js
@@ -1,14 +1,14 @@
 
 //  ドロワーメニュー
-const jsDrawerButton = document.getElementById('js-drawer');
+const drawerButton = document.getElementById('js-drawer');
 const body = document.body;
 const spGlobalNav = document.getElementById('js-global-nav');
 const headerLogo = document.getElementById('js-header-logo');
 
-jsDrawerButton.addEventListener('click', function () {
+drawerButton.addEventListener('click', function () {
   body.classList.toggle('is-drawerActive');
   headerLogo.classList.toggle('is-active');
-  if (jsDrawerButton.getAttribute('aria-expanded') == 'false') {
+  if (drawerButton.getAttribute('aria-expanded') == 'false') {
     this.setAttribute('aria-expanded', 'true');
     this.classList.add('is-active');
     spGlobalNav.setAttribute('area-hidden', 'false');
