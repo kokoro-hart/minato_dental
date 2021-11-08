824c09ac62f3ee7623248b7c5f0aca2379455029
kokoro <cocosyu.11261126@gmail.com>
2021-11-08 20:06:08 +0900

    Add: お問い合わせ完了ページを追加

---
+++ wp/page-contact-thanks.php
@@ -0,0 +1,49 @@
+    <?php get_header(); ?>
+
+    <main class="page-contact">
+      <!--メインビジュアル-->
+      <section class="p-lower-mv">
+        <picture class="p-lower-mv__picture">
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-contact_sp.webp" media="(max-width: 767px)" type="image/webp" />
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-contact_pc.webp" media="(min-width: 768px)" type="image/webp" />
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/common/mv-contact_sp.jpeg" media="(max-width: 767px)" />
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/mv-contact_pc.jpeg" class="p-lower-mv__img" media="(min-width: 768px)" decoding="async" alt="お問い合わせ">
+        </picture>
+        <div class="p-lower-mv__contents">
+          <h2 class="p-lower-mv__heading">お問い合わせ</h2>
+          <p class="p-lower-mv__lead">CONTACT</p>
+        </div>
+      </section>
+      <!--/メインビジュアル-->
+      <!--パンくずリスト-->
+      <div class="c-breadcrumb u-mt10">
+        <div class="l-inner">
+          <ul class="c-breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/"><span itemprop="name">ホーム</span></a>
+              <meta itemprop="position" content="1" />
+            </li>
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/contact/"><span itemprop="name">お問い合わせ</span></a>
+              <meta itemprop="position" content="2" />
+            </li>
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/contact-thanks/"><span itemprop="name">お問い合わせ完了</span></a>
+              <meta itemprop="position" content="3" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--/パンくずリスト-->
+      <section class="p-contact l-lower">
+        <div class="l-inner">
+          <p class="p-contact__intro">
+            お問い合わせありがとうございました。<br>
+            3営業日以内に返信いたしますので、しばらくお待ちいただけますと幸いです。<br>
+            <span class="u-color-red">※3営業日以内に当院からの返信がない場合には、お電話(<a href="tel:0312345678">TEL03-1234-5678</a>)にてお問い合わせ下さい。</span>
+          </p>
+        </div>
+      </section>
+    </main>
+    
+    <?php get_footer(); ?>
