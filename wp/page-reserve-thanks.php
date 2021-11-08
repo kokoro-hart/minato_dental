2441a28ca837d49e3d7970fc778107c30c6bda8a
kokoro <cocosyu.11261126@gmail.com>
2021-11-08 20:06:39 +0900

    Add: 予約完了ページを追加

---
+++ wp/page-reserve-thanks.php
@@ -0,0 +1,49 @@
+    <?php get_header(); ?>
+
+    <main class="p-reserve">
+      <!--メインビジュアル-->
+      <section class="p-lower-mv">
+        <picture class="p-lower-mv__picture">
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-contact_sp.webp" media="(max-width: 767px)" type="image/webp" />
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-contact_pc.webp" media="(min-width: 768px)" type="image/webp" />
+          <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/common/mv-contact_sp.jpeg" media="(max-width: 767px)" />
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/mv-contact_pc.jpeg" class="p-lower-mv__img" media="(min-width: 768px)" decoding="async" alt="WEB予約">
+        </picture>
+        <div class="p-lower-mv__contents">
+          <h2 class="p-lower-mv__heading">WEB予約</h2>
+          <p class="p-lower-mv__lead">RESERVE</p>
+        </div>
+      </section>
+      <!--/メインビジュアル-->
+      <!--パンくずリスト-->
+      <div class="c-breadcrumb u-mt10">
+        <div class="l-inner">
+          <ul class="c-breadcrumb__list" itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/"><span itemprop="name">ホーム</span></a>
+              <meta itemprop="position" content="1" />
+            </li>
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/reserve/"><span itemprop="name">WEB予約</span></a>
+              <meta itemprop="position" content="2" />
+            </li>
+            <li class="c-breadcrumb__item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="c-breadcrumb__link" itemprop="item" href="/reserve-thanks/"><span itemprop="name">WEB予約完了</span></a>
+              <meta itemprop="position" content="3" />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!--/パンくずリスト-->
+      <section class="p-contact l-lower">
+        <div class="l-inner">
+          <p class="p-contact__intro">
+            WEBよりご予約いただき誠にありがとうございます。<br>
+            送信いただいた内容を確認して1営業日以内に返信いたします。<br>
+            <span class="u-color-red">※1営業日以内に当院からの返信がない場合には、お電話(<a href="tel:0312345678">TEL03-1234-5678</a>)にてお問い合わせ下さい。</span>
+          </p>
+        </div>
+      </section>
+    </main>
+
+    <?php get_footer(); ?>
