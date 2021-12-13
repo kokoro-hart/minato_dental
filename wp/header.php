ac8d4c89c7ceb10d2c868281a5c02cdaf3a2141c
kokoro <cocosyu.11261126@gmail.com>
2021-12-13 22:48:46 +0900

    Add: favicon apple-touch-iconを追加

--- wp/header.php
+++ wp/header.php
@@ -5,13 +5,14 @@
   <meta name="format-detection" content="telephone=no">
   <meta name="viewport" content="width=device-width">
   <link rel="apple-touch-icon" href="<? echo get_template_directory_uri(); ?>/img/common/apple-touch-icon.png">
+  <link rel="icon" href="<? echo get_template_directory_uri(); ?>/img/common/favicon.ico" id="favicon">
   <meta name="description" content="南町の歯医者さん">
   <meta property="og:title" content="みなみ歯科クリニック">
   <meta property="og:description" content="南町の歯医者さん">
   <meta property="og:url" content="サイトのドメイン/">
   <meta property="og:type" content="website ">
   <meta property="og:local" content="ja_JP">
-  <meta property="og:image" content="/common/og-image.jpg">
+  <meta property="og:image" content="<? echo get_template_directory_uri(); ?>/img/common/mv01.jpeg">
   <meta property="og:site_name" content="サイトのドメイン ">
 
   <?php wp_head(); ?>
@@ -32,7 +33,7 @@
         <!--/ヘッダーロゴ-->
         <div class="l-header__content">
           <!--グローバルナビ-->
-          <nav class="p-global-nav" id="js-global-nav" aria-level="ページ内のメニュー">
+          <nav class="p-global-nav" id="js-global-nav">
             <ul class="p-global-nav__list">
               <li class="p-global-nav__item">
                 <a href="<?php echo esc_url(home_url('/')); ?>" class="p-global-nav__link <?php if(is_front_page()) echo 'is-current' ?>">
@@ -116,7 +117,7 @@
           </nav>
           <!--/グローバルナビ-->
           <!--ドロワーボタン-->
-          <button id="js-drawer" class="c-button-drawer u-hidden-xl-up" aria-controls="js-glabal-nav" aria-expanded="false" area-label="メニューを開閉する">
+          <button id="js-drawer" class="c-button-drawer u-hidden-xl-up" aria-controls="js-global-nav" aria-expanded="false">
             <span class="c-button-drawer__line"></span>
             <span class="c-button-drawer__line"></span>
             <span class="c-button-drawer__line"></span>
