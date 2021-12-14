c99add92651bc35a8da51a005d9807340791d3b4
kokoro <cocosyu.11261126@gmail.com>
2021-12-14 22:06:18 +0900

    Fix:クラス名を修正

--- wp/taxonomy.php
+++ wp/taxonomy.php
@@ -1,14 +1,14 @@
 
     <?php get_header(); ?>
 
-    <main class="p-news">
+    <main>
       <!--メインビジュアル-->
       <section class="p-lower-mv">
         <picture class="p-lower-mv__picture">
           <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-blog_sp.webp" media="(max-width: 767px)" type="image/webp" />
           <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/webp/mv-blog_pc.webp" media="(min-width: 768px)" type="image/webp" />
           <source class="p-lower-mv__img" srcset="<?php echo get_template_directory_uri(); ?>/img/common/mv-blog_sp.jpeg" media="(max-width: 767px)" />
-          <img src="<?php echo get_template_directory_uri(); ?>/img/common/mv-blog_pc.jpeg" class="p-lower-mv__img" media="(min-width: 768px)" decoding="async" alt="スタッフブログ">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/common/mv-blog_pc.jpeg" class="p-lower-mv__img" media="(min-width: 768px)" decoding="async" alt="お知らせ">
         </picture>
         <div class="p-lower-mv__contents">
           <h2 class="p-lower-mv__heading">お知らせ</h2>
@@ -56,28 +56,28 @@
                 foreach($custom_posts as $post): setup_postdata($post); 
               ?>                  
 
-              <a href="<?php the_permalink(); ?>" class="p-archive-main-card">
-                <div class="p-archive-main-card__thumbnail">
+              <a href="<?php the_permalink(); ?>" class="p-archive-main__card p-card-archive">
+                <div class="p-card-archive__thumbnail">
                   <?php
                     if(has_post_thumbnail()) {
                       the_post_thumbnail('small', array(
-                        'class' => 'p-archive-main-card__img lazyload'
+                        'class' => 'p-card-archive__img lazyload'
                       ));
                     } else {
-                      echo '<img data-src="' . esc_url(get_template_directory_uri()) . '/img/common/blog01.jpeg" alt="記事の画像" class="p-archive-main-card__img lazyload">';
+                      echo '<img data-src="' . esc_url(get_template_directory_uri()) . '/img/common/blog01.jpeg" alt="記事の画像" class="p-card-archive__img lazyload">';
                     }
                   ?>
                 </div>
-                <div class="p-archive-main-card__body">
-                  <div class="p-archive-main-card__cats">
-                    <p class="p-archive-main-card__cat">
+                <div class="p-card-archive__body">
+                  <div class="p-card-archive__cats">
+                    <p class="p-card-archive__cat">
                       <?php echo esc_html( get_the_terms( get_the_ID(), 'genre' )[0]->name ); ?>
                     </p>
                   </div>
-                  <h3 class="p-archive-main-card__title">
+                  <h3 class="p-card-archive__title">
                     <?php the_title(); ?>
                   </h3>
-                  <time class="p-archive-main-card__time" datetime="<?php the_time('c'); ?>">
+                  <time class="p-card-archive__time" datetime="<?php the_time('c'); ?>">
                     <?php the_time('Y.n.j'); ?>
                   </time>
                 </div>
